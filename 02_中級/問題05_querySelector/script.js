7ff1fe790348aa29fd63637fbdc6366f8a63e30a
dongaiji <KajiyamaKouhei61@gail.com>
2026-07-08 15:49:06 +0900

    中級問題５終了

--- 02_中級/問題05_querySelector/script.js
+++ 02_中級/問題05_querySelector/script.js
@@ -1,2 +1,12 @@
-// ここに JavaScript を書いてください
+const el = document.querySelector('#title');
+el.textContent = "書き換えられました！";
 
+const el2 = document.querySelector('.description');
+el2.textContent = "JavaScriptでDOMを書き換えた結果です。";
+
+const items = document.querySelectorAll('.item');
+const letters = ['A', 'B', 'C',];
+
+items.forEach((item, index) => {
+    item.textContent = letters[index];
+});
